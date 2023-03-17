a4b28cf5fb0e4d6f883e5130c0b283e212333fa8
AthanasiaPapadopoulou <61753837+AthanasiaPapadopoulou@users.noreply.github.com>
2023-03-17 23:38:46 +0200

    Add files via upload

---
+++ Hangman in Java/src/Menu.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+
+public class Menu {
+     private char choice;
+     Scanner keyboard = new Scanner(System.in);
+     
+	 public Menu() {// constructor tis Menu
+		 choice=0;
+	 }
+     public Menu(char choicee)// i epilogi tou xristi gia to menu
+     {
+    	 choice=choicee;
+     }
+     
+     public char getChoice() {
+    	 return choice;
+     }
+     
+     public void read()
+	{
+		System.out.print("MAIN MENU \n -Start a new Game (G) \n -Statistics (S) \n -Exit (E) \n Please enter your choice: ");
+		choice = keyboard.next().charAt(0);
+		
+	}
+     
+     public boolean rightchoice() {
+    	 if (choice=='G'||choice=='g'||choice=='S'||choice=='s'||choice=='E'||choice=='e')
+    		 return true;
+    	 else 
+    		 {System.out.println("None of the stated choices was selected. Please type your choice again. ");
+             return false;
+    		 }
+     }
+}
+
